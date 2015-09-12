d250449888076ce5c419baee65d221799986bad6
Pedro Howat <pedro.howat@gmail.com>
2015-09-12 02:11:24 -0300

    Testing user signup

--- cloud/app.js
+++ cloud/app.js
@@ -1,6 +1,8 @@
 
 // These two lines are required to initialize Express in Cloud Code.
  express = require('express');
+ var parseExpressHttpsRedirect = require('parse-express-https-redirect');
+ var parseExpressCookieSession = require('parse-express-cookie-session');
  app = express();
 
 // Global app configuration section
@@ -14,6 +16,45 @@ app.get('/hello', function(req, res) {
   res.render('hello', { message: 'Congrats, you just set up your app!' });
 });
 
+app.post('/hello', function (req, res) {
+Parse.User.logIn(req.body.username, req.body.password).then(function() {
+      // Login succeeded, redirect to homepage.
+      // parseExpressCookieSessio n will automatically set cookie.
+      console.log("DELETEME: LOGGED IN");
+      res.redirect('/hello');
+    },
+    function(error) {
+      // Login failed, redirect back to login form.
+      console.log("DELETEME: LOGIN ERROR", error);
+      console.log(error);
+      res.redirect('/hello');
+    });
+});
+
+app.get('/hello', function(req, res) {
+  res.render('hello', { message: 'Congrats, you just set up your app!' });
+});
+
+app.get('/signup', function(req, res) {
+  res.render('signup');
+});
+
+app.post('/signup', function (req, res) {
+    var newUser = new Parse.User();
+    newUser.set("username", req.body.username);
+    newUser.set("password", req.body.password);
+    newUser.signUp(null, {
+        success: function(user) {
+            console.log("DELETEME: USER CREATED");
+        },
+        error: function(user, error) {
+            console.log("DELETEME: Some error happened");
+            console.log(error);
+        // Show the error message somewhere and let the user try again.
+        alert("Error: " + error.code + " " + error.message);
+        }
+    });
+});
 // // Example reading from the request query string of an HTTP get request.
 // app.get('/test', function(req, res) {
 //   // GET http://example.parseapp.com/test?message=hello
