8a17e71993681b75153524355800a90ea3eeaf70
guilhermekrz <guilhermekrz@gmail.com>
2015-11-20 17:33:22 -0200

    Added more push capabilities

--- cloud/main.js
+++ cloud/main.js
@@ -2,44 +2,69 @@ require('cloud/app.js');
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+  	response.success("Hello world!");
 });
 
 // Send push to users from determined location
+// TODO: add more fields: https://www.parse.com/docs/js/guide#push-notifications-sending-options
 Parse.Cloud.define("sendPushToLocation", function(request, response) {
-  var senderUser = request.user;
-  var recipientUserId = request.params.recipientId;
-  var message = request.params.message;
+  	var userObjectId = request.params.userObjectId;
 
-  // Validate that the sender is allowed to send to the recipient.
-  // For example each user has an array of objectIds of friends
-  /*if (senderUser.get("friendIds").indexOf(recipientUserId) === -1) {
-    response.error("The recipient is not the sender's friend, cannot send push.");
-  }*/
+  	// FIXME: this is only temporary. We need to find a way to do this with roles!
+  	if(userObjectId != "dpbBxLtkMY" && userObjectId != "4r1i9gZkYD") {
+  		response.error("Permission denied! User is not an admin.");
+  		return;
+  	}
+  
+	var latitude = request.params.latitude;
+	var longitude = request.params.longitude;
+	if(latitude != null && longitude != null) {
+		var locationGeoPoint = new Parse.GeoPoint({latitude: latitude, longitude: longitude});
+	}
 
-  // Validate the message text.
-  // For example make sure it is under 140 characters
-  if (message.length > 140) {
-  // Truncate and add a ...
-    message = message.substring(0, 137) + "...";
-  }
+	// Validate the message text.
+	var message = request.params.message;
+	
+	// Make sure it is under 140 characters
+	if (message.length > 140) {
+		// Truncate and add a ...
+		message = message.substring(0, 137) + "...";
+	}
 
-  // Send the push.
-  // Find devices associated with the recipient user
-  var recipientUser = new Parse.User();
-  recipientUser.id = recipientUserId;
-  var pushQuery = new Parse.Query(Parse.Installation);
-  pushQuery.equalTo("user", recipientUser);
- 
-  // Send the push notification to results of the query
-  Parse.Push.send({
-    where: pushQuery,
-    data: {
-      alert: message
-    }
-  }).then(function() {
-      response.success("Push was sent successfully.")
-  }, function(error) {
-      response.error("Push failed to send with error: " + error.message);
-  });
+	var title = request.params.title;
+	var uri = request.params.uri;
+	var recipientUserId = request.params.recipientId;
+	
+	// Push query
+	var pushQuery = new Parse.Query(Parse.Installation);
+
+	if(recipientUserId != null) {
+		var recipientUser = new Parse.User();
+		recipientUser.id = recipientUserId;
+		pushQuery.equalTo("user", recipientUser);
+	}
+	else if(locationGeoPoint != null) {
+		// Find users near given location (100 miles)
+		var userQuery = new Parse.Query(Parse.User);
+		userQuery.withinMiles("location", locationGeoPoint, 100.0);
+		pushQuery.matchesQuery('user', userQuery);
+		// TODO: confirm that this works with more than 1000 users!
+
+		response.error("Permission denied! User is not an admin.");
+  		return;
+	}	
+
+	// Send the push notification to results of the query
+	Parse.Push.send({
+		where: pushQuery,
+		data: {
+	 		alert: message, // the notification's message.
+	 		title: title, //(Android, Windows 8, & Windows Phone 8 only) the value displayed in the Android system tray or Windows 8 toast notification.
+	 		uri: uri // (Android only) an optional field that contains a URI. When the notification is opened, an Activity associated with opening the URI is launched.
+		}
+	}).then(function() {
+	 	response.success("Push foi enviado com sucesso!");
+	}, function(error) {
+	 	response.error("Falha ao enviar o push: " + error.message);
+	});
 });
